dcb5f7d5455bf8387d319ddb05ea6a4125e219a8
Alessandroblas <blasettiale@gmail.com>
2021-02-11 23:18:25 +0100

    lab2

--- Lab2/bulk-xy.js
+++ Lab2/bulk-xy.js
@@ -1,8 +1,9 @@
 var c = db.testcoll.initializeUnorderedBulkOp()
+var N = 1000;
 
-for (var i=0;i<1000;i++) {
+for (var i=0;i<N;i++) {
 
-	c.insert( { i : i } );
+	c.insert( { x : i, y : N-i } );
 }
 
 var ex =  c.execute();
